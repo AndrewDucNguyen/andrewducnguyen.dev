8775f904d9c37487e9b008f29894502cc7e9be3b
Andrew Nguyen <anguyen0615@gmail.com>
2024-01-26 17:48:56 -0600

    Social media icon
    - Added social media icon to Header component
    - Installed Lucide react package to utilize icons
    - Included in Footer component

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Github, Instagram, Linkedin } from 'lucide-react'
 
 const Footer = () => {
     return (
-        <div className='mx-auto w-full px-16 mt-8 mb-16'>
-            <a className='font-bold' href="/">Andrew<span className='font-normal'>Duc</span>Nguyen</a>
+        <div className='mx-auto w-full px-20 mt-8 mb-16'>
+            <div className='flex justify-between'>
+                <a className='font-bold' href="/">Andrew<span className='font-normal'>Duc</span>Nguyen</a>
+                <div className='flex gap-x-6'>
+                    <a href='https://www.instagram.com/_andrewducnguyen/' target="_blank" rel="noopener noreferrer">
+                        <Instagram color='#d4d4d4' />
+                    </a>
+                    <a href='https://www.linkedin.com/in/andrew-nguyen-b4038912b/' target="_blank" rel="noopener noreferrer">
+                        <Linkedin color='#d4d4d4' />
+                    </a>
+                    <a href='https://github.com/AndrewDucNguyen' target="_blank" rel="noopener noreferrer">
+                        <Github color='#d4d4d4' />
+                    </a>
+                </div>
+            </div>
+
+
         </div>
     )
 }
--- src/app/component/Header.jsx
+++ src/app/component/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Github, Instagram, Linkedin } from 'lucide-react'
 
 const Header = () => {
     return (
-        <div className='mx-auto w-full px-16 mt-16'>
-            <a className='font-bold' href="/">Andrew<span className='font-normal'>Duc</span>Nguyen</a>
+        <div className='mx-auto w-full px-20 mt-16'>
+            <div className='topHeader mb-16'>
+                <a className='font-bold' href="/">Andrew<span className='font-normal'>Duc</span>Nguyen</a>
+            </div>
+            <div className='bottomHeader flex flex-row justify-between'>
+                <div className='flex gap-x-6'>
+                    <a href='https://www.instagram.com/_andrewducnguyen/' target="_blank" rel="noopener noreferrer">
+                        <Instagram color='#d4d4d4' />
+                    </a>
+                    <a href='https://www.linkedin.com/in/andrew-nguyen-b4038912b/' target="_blank" rel="noopener noreferrer">
+                        <Linkedin color='#d4d4d4' />
+                    </a>
+                    <a href='https://github.com/AndrewDucNguyen' target="_blank" rel="noopener noreferrer">
+                        <Github color='#d4d4d4' />
+                    </a>
+                </div>
+                <hr className='max-w-80 h-[.05rem] w-full border-none bg-[#d4d4d4]' />
+            </div>
         </div>
     )
 }
