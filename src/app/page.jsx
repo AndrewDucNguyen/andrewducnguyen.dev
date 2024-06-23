f2365e07befd9056c873900f73a53e1d5fd2f30f
Andrew Nguyen <anguyen0615@gmail.com>
2024-06-22 22:06:07 -0500

    Updates
    - Update packages
    - Update homescreen

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="h-screen">
       <div className="text-center mt-20">
         <h1 className="text-4xl font-bold">Coming Soon</h1>
         <p className="mt-4 text-gray-600">The website is under construction. Stay tuned for updates!</p>
